5ba527de569813e405ab5282650eea7425b1734c
brahim-driouch <brahimdriouch.dev@gmail.com>
2024-07-03 21:04:25 +0100

    working on auth middleware

--- app/[locale]/(protected)/profile/page.tsx
+++ app/[locale]/(protected)/profile/page.tsx
@@ -1,31 +1,12 @@
-import UserInfo from "@/app/_components/profile/UserInfo"
-import { getSession } from "@/auth"
-import { LoggedInUserType } from "@/dataSchemas"
-import { getLocale } from "next-intl/server"
-import Link from "next/link"
 
 
 
 
-export default async function ProfilePage() {
+export default async function ProfilePgae() {
 
-    const user :LoggedInUserType= (await getSession())?.user
-    const locale = await getLocale()
-
-   
-
-    if(!user?.isVerified){
-        return (
-            <div>
-                Merci de bien vouloir verifier votre compte <Link href={`/${locale}/account-validation?uid=${user?.id}`}></Link>
-            </div>
-        )
-    }
-
-  return (
-    <div>
-        <UserInfo/>
-    </div>
-  )
+    return (
+        <div>
+                prfile
+        </div>
+    )
 }
-
--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,61 @@
 import createMiddleware from 'next-intl/middleware';
+import { NextRequest, NextResponse } from 'next/server';
+import { getSession } from './auth';
 
 
-export default createMiddleware({
+const protectedRoutes = [
+  '/profile'
+]
+
+
+const i18nConfig = {
   // A list of all locales that are supported
   locales: ['ar', 'fr'],
- 
   // Used when no locale matches
-  defaultLocale: 'fr'
-});
+  defaultLocale: 'fr',
+  
+}
+async function authMiddleware(request:NextRequest){
+  const auth = await getSession()
+  const isProtected = protectedRoutes.some(route=>request.nextUrl.pathname.includes(route))
+
+  console.log(auth,isProtected)
+
+ if(isProtected && !auth?.user){
+   return NextResponse.redirect(new URL(`/${i18nConfig.defaultLocale}/se-connecter`,request.nextUrl))
+ }
+
+ NextResponse.next()
+
+
+  
+}
+
+
+
+
+export default async function middleware(request:NextRequest){
+  await authMiddleware(request)
+
+  const defaultLocale = request.headers.get('x-your-custom-locale') || 'en';
+ 
+  // Step 2: Create and call the next-intl middleware (example)
+  const handleI18nRouting = createMiddleware(i18nConfig);
+  const response = handleI18nRouting(request);
+ 
+  // Step 3: Alter the response (example)
+  response.headers.set('x-hrayfi_locale', defaultLocale);
+
+
+  return response
+}
+
+// export async function middleware(request:Request){
+//   const locale =  'fr' || 'ar'
+
+//   const url = request.url
+//   console.log(url)
+// }
   
 export const config = {
   // Match only internationalized pathnames
