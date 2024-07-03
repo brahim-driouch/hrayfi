6f7f5b0e1f772e291d958960756799a103bf6c1c
brahim-driouch <brahimdriouch.dev@gmail.com>
2024-07-03 21:13:39 +0100

    i18n and auth middleware works

--- middleware.ts
+++ middleware.ts
@@ -19,14 +19,8 @@ async function authMiddleware(request:NextRequest){
   const auth = await getSession()
   const isProtected = protectedRoutes.some(route=>request.nextUrl.pathname.includes(route))
 
-  console.log(auth,isProtected)
 
- if(isProtected && !auth?.user){
-   return NextResponse.redirect(new URL(`/${i18nConfig.defaultLocale}/se-connecter`,request.nextUrl))
- }
-
- NextResponse.next()
-
+ return isProtected && !auth?.user
 
   
 }
@@ -35,19 +29,23 @@ async function authMiddleware(request:NextRequest){
 
 
 export default async function middleware(request:NextRequest){
-  await authMiddleware(request)
+  const authMiddlewaResponse = await authMiddleware(request)
 
-  const defaultLocale = request.headers.get('x-your-custom-locale') || 'en';
+  if(authMiddlewaResponse){
+    return NextResponse.redirect(new URL(`/${i18nConfig.defaultLocale}/se-connecter`,request.nextUrl))
+
+  }
+
+  // const defaultLocale = request.headers.get('x-your-custom-locale') || 'en';
  
   // Step 2: Create and call the next-intl middleware (example)
   const handleI18nRouting = createMiddleware(i18nConfig);
-  const response = handleI18nRouting(request);
+    return  handleI18nRouting(request);
  
-  // Step 3: Alter the response (example)
-  response.headers.set('x-hrayfi_locale', defaultLocale);
+  // // Step 3: Alter the response (example)
+  // response.headers.set('x-hrayfi_locale', defaultLocale);
 
 
-  return response
 }
 
 // export async function middleware(request:Request){
